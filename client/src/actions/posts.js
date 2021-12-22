def0a1aea5bd2b6dd568b4af8c2f89893abf74b2
Saloni <subedisaloni2002@gmail.com>
2021-12-22 13:45:03 +0530

    add action constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,16 +1,22 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
 import * as api from "../api";
 
 // action creators-> functions that returns(dispatch) action
 // action-> type + payload
 // payload-> data to store all posts.
 
-
 // const action = {type: 'FETCH_ALL', payload: []}
 // dispatch(action);
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (err) {
     console.log(err);
   }
@@ -19,7 +25,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (err) {
     console.log(err);
   }
@@ -28,7 +34,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (err) {
     console.log(err);
   }
@@ -37,7 +43,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (err) {
     console.log(err);
   }
@@ -45,10 +51,9 @@ export const deletePost = (id) => async (dispatch) => {
 
 export const likePost = (id) => async (dispatch) => {
   try {
-    const {data} = await api.likePost(id);
-    dispatch({ type: "LIKE", payload: data });
-
+    const { data } = await api.likePost(id);
+    dispatch({ type: LIKE, payload: data });
   } catch (err) {
     console.log(err);
   }
-}
+};
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,24 @@
 //Reducer is a function which accepts state and action and returns the changed state.
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  DELETE,
+  LIKE,
+} from "../constants/actionTypes";
 
 export default (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
-    case "LIKE":
-    case "UPDATE":
+    case LIKE:
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload; //action.payload are posts only
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload]; //previous posts and new post
     default:
       return posts;
